0ae9ee35ae9bd594c89037547b2bd69bb2d75a95
yangwenqiang <yangwenqiang@163.com>
2014-12-30 09:26:28 +0800

    format output file

--- readELF.go
+++ readELF.go
@@ -26,13 +26,40 @@ func parseELF(in string, out string) {
 	}
 
 	// read input file content
-	buf := make([]byte, 1024)
+	buf := make([]byte, 16)
+  // line number
+  i := 0
 	for {
+    // line number
+    i++
 		n, _ := fin.Read(buf)
 		if 0 == n {
 			break
 		}
 		// write into outfile
-		fout.Write(buf[0:n])
+    outputLine(fout, i, buf[:n])
 	}
+}
+
+// output fomate one line
+func outputLine(f *os.File, row int, data []byte){
+		// line scope
+    line := fmt.Sprintf("%04X:%04X", row, row*16)
+		f.WriteString(line)
+
+    // split
+    f.WriteString(" | ")
+
+		// 16 hex content
+    length := len(data)
+		content := fmt.Sprintf("%X", data[:length])
+		f.WriteString(content)
+
+    // split
+    f.WriteString(" | ")
+
+		// string content
+    f.Write(data[:length])
+
+    f.WriteString("\n")
 }
