79281a7502f12cd7331a7397147d9c8d9a149d07
leowenyang <leowenyang@163.com>
2014-12-31 08:17:18 +0800

    fixed output file line number error

--- readELF.go
+++ readELF.go
@@ -27,39 +27,39 @@ func parseELF(in string, out string) {
 
 	// read input file content
 	buf := make([]byte, 16)
-  // line number
-  i := 0
+	// line number
+	i := 0
 	for {
-    // line number
-    i++
+		// line number
+		i++
 		n, _ := fin.Read(buf)
 		if 0 == n {
 			break
 		}
 		// write into outfile
-    outputLine(fout, i, buf[:n])
+		outputLine(fout, i, buf[:n])
 	}
 }
 
 // output fomate one line
-func outputLine(f *os.File, row int, data []byte){
-		// line scope
-    line := fmt.Sprintf("%04X:%04X", row, row*16)
-		f.WriteString(line)
+func outputLine(f *os.File, row int, data []byte) {
+	// line scope
+	line := fmt.Sprintf("%04X:%04X", (row-1)*16, row*16-1)
+	f.WriteString(line)
 
-    // split
-    f.WriteString(" | ")
+	// split
+	f.WriteString(" | ")
 
-		// 16 hex content
-    length := len(data)
-		content := fmt.Sprintf("%X", data[:length])
-		f.WriteString(content)
+	// 16 hex content
+	length := len(data)
+	content := fmt.Sprintf("%X", data[:length])
+	f.WriteString(content)
 
-    // split
-    f.WriteString(" | ")
+	// split
+	f.WriteString(" | ")
 
-		// string content
-    f.Write(data[:length])
+	// string content
+	f.Write(data[:length])
 
-    f.WriteString("\n")
+	f.WriteString("\n")
 }
